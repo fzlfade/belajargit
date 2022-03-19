5a88f0249d0f7c3b1befae48eb235f2e746c5165
fzlfade <faizal210505@gmail.com>
2022-03-19 09:35:42 +0700

    coba

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<h2>Mekanisme Asidifikasi Samudera</h2>
+<p>Proses asidifikasi samudra, secara sederhana adalah karbon dioksida 
+dari pembakaran bahan bakar fosil yang terakumulasi dalam atmosfer, 
+menyebabkan pemanasan global, berpengaruh terhadap samudra atau 
+lautan kita. karbon dioksida diserap oleh laut dan bereaksi dengan air laut 
+membentuk asam karbonat H2CO3 dan meningkatkan keasamam (H+) air 
+laut. Lebih jelasnya perhatikan gambar berikut ini:</p>
+<figure>
+ <img src="<?php echo base_url('img/george.png');?>" alt="Mekanisme asidifikasi samudra" width="300px"
+height="200px">
+ <figcaption>Gambar 1. Mekanisme Asidifikasi Samudera</figcaption>
+</figure>
+<p>Sumber: University of Maryland</p>
